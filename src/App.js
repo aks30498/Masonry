e0dc2c4ef42eaaaabc3d32829a30b7b079e71bc9
aks30498 <aksharma30498@gmail.com>
2021-04-11 14:39:28 +0530

    fixed effect error

--- src/App.js
+++ src/App.js
@@ -6,40 +6,30 @@ import unsplash from "./unsplash";
 import InfiniteScroll from "react-infinite-scroll-component";
 
 function App() {
-  const [images, setImages] = useState([]);
+  const [images, setImages] = useState(undefined);
   const [overlayImages, setOverlayImages] = useState(undefined);
-  let page = 1;
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     unsplash.search
       .getPhotos({
         query: "cat",
         page,
-        perPage: 30,
-        color: "green",
+        perPage: 30,       
         orientation: "portrait",
       })
-      .then((result) => setImages(result.response.results.map((obj) => obj.urls.small)))
+      .then((result) =>
+        setImages((images) =>
+          images
+            ? [
+                ...images,
+                ...result.response.results.map((obj) => obj.urls.small),
+              ]
+            : result.response.results.map((obj) => obj.urls.small)
+        )
+      )
       .catch((err) => console.log(err));
-
-    // setTimeout(() => {
-    //   setImages(dummyResponse.map((obj) => obj.urls.small));
-    // }, 1000);
-  }, []);
-
-  const fetchData = () => {
-    page = page+1
-    unsplash.search
-      .getPhotos({
-        query: "cat",
-        page,
-        perPage: 20,
-        color: "green",
-        orientation: "portrait",
-      })
-      .then((result) => setImages([...images, ...result.response.results.map((obj) => obj.urls.small)]))
-      .catch((err) => console.log(err));
-  };
+  }, [page]);
 
   return (
     <>
@@ -57,36 +47,39 @@ function App() {
         </div>
 
         {/* Gallery section */}
-        <div className="gallerySectionWrapper">
-          <InfiniteScroll
-            dataLength={images.length} //This is important field to render the next data
-            next={fetchData}
-            hasMore={true}
-            loader={<div className="loading">Getting more kitties ...</div>}
-            endMessage={
-              <p style={{ textAlign: "center" }}>
-                <b>No more Images!</b>
-              </p>
-            }
-          >
-            <div className="galleryContainer">
-              {images.map((img, index) => (
-                <div
-                  key={`img-${index}`}
-                  className="imgWrapper"
-                  onClick={() =>
-                    setOverlayImages({
-                      images,
-                      index,
-                    })
-                  }
-                >
-                  <img className="img" alt="img" src={img} />
-                </div>
-              ))}
-            </div>
-          </InfiniteScroll>
-        </div>
+        {images && (
+          <div className="gallerySectionWrapper">
+            <InfiniteScroll
+              dataLength={images ? images.length : 0}
+              next={() => setPage((page) => page + 1)}
+              hasMore={true}
+              loader={<div className="loading">Getting more kitties ...</div>}
+              endMessage={
+                <p style={{ textAlign: "center" }}>
+                  <b>No more Images!</b>
+                </p>
+              }
+            >
+              <div className="galleryContainer">
+                {images &&
+                  images.map((img, index) => (
+                    <div
+                      key={`img-${index}`}
+                      className="imgWrapper"
+                      onClick={() =>
+                        setOverlayImages({
+                          images,
+                          index,
+                        })
+                      }
+                    >
+                      <img className="img" alt="img" src={img} />
+                    </div>
+                  ))}
+              </div>
+            </InfiniteScroll>
+          </div>
+        )}
       </div>
     </>
   );
